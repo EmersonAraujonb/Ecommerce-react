10e342d74b8688eee920836edc592e1bfc2124c7
Emerson Araujo <emersonsantos0302@gmail.com>
2023-06-29 13:02:56 -0300

    feat: update

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Image from '../../assets/about.jpg'
 
 const About = () => {
     return (
-        <section >
-        <div >
-          <div  >
-            <h1 >About
+        <section className="text-gray-600 body-font cards">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">About
             </h1>
-            <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nihil id animi iste enim, quidem ut ea aspernatur consectetur, quisquam asperiores harum! Quod veniam nihil accusamus natus reprehenderit aperiam. Vero.</p>
+            <p className="mb-8 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nihil id animi iste enim, quidem ut ea aspernatur consectetur, quisquam asperiores harum! Quod veniam nihil accusamus natus reprehenderit aperiam. Vero.</p>
           </div>
-          <div  >
-            <img alt="hero" src="../../src/assets/about.png"/>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img className="object-cover object-center rounded" alt="hero" src={Image}/>
           </div>
         </div>
       </section>
