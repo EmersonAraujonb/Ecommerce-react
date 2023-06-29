6a6b0da0dd2b99f64651e366a0391aa4d3051c19
Emerson Araujo <emersonsantos0302@gmail.com>
2023-06-29 11:43:02 -0300

    feat: update indexs

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,34 +1,33 @@
 import './App.css'
-import Footer from './components/Footer'
-// import Header from './components/Header'
-import Home from './modules/Home'
+import Footer from './components/Footer/index'
+import Header from './components/Header/index'
+import Home from './modules/Home/index'
 import { Routes, Route } from 'react-router-dom'
-import Product from './modules/Product'
-import Products from './modules/Products'
+import Product from './modules/Product/index'
+import Products from './modules/Products/index'
 import CategoryProducts from './modules/CategoryProducts/index,'
-import Cart from './modules/Cart'
-import About from './components/About'
-import Contact from './components/Contact'
+import Cart from './modules/Cart/index'
+import About from './components/About/index'
+import Contact from './components/Contact/index'
 
 function App() {
 
   return (
 
-    <div>
-      {/* <Header /> */}
+    <>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/products/:id' element={<Product/>} />
-        <Route path='/products' element={<Products/>} />
-        <Route path='/categories/:name' element={<CategoryProducts/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/cart' element={<Cart/>} />
+        <Header />
+        <Route path='/' element={<Home />} />
+        <Route path='/products/:id' element={<Product />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/categories/:name' element={<CategoryProducts />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
         <Route path='*' element={<div>Not Found 404</div>} />
+        <Footer />
       </Routes>
-      <Footer />
-    </div>
-
+    </>
   )
 }
 
