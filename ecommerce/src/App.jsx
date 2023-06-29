28f23f8edb4ccea4a52c95ba562361e094e35d08
Emerson Araujo <emersonsantos0302@gmail.com>
2023-06-29 11:35:43 -0300

    feat: update

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import Footer from './components/Footer'
-import Header from './components/Header'
+// import Header from './components/Header'
 import Home from './modules/Home'
 import { Routes, Route } from 'react-router-dom'
 import Product from './modules/Product'
 import Products from './modules/Products'
-import CategoryProducts from './modules/CategoryProducts/index,'
+import CategoryProducts from './modules/CategoryProducts'
 import Cart from './modules/Cart'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -15,7 +15,7 @@ function App() {
   return (
 
     <div>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/products/:id' element={<Product/>} />
